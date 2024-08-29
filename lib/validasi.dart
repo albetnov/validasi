918a85c850f89bdde5faf895876ead71c37ec820
albetnov <ryxinfrvr@gmail.com>
2024-08-29 21:11:19 +0700

    chore: extract individual class from `form` instead.

--- lib/validasi.dart
+++ lib/validasi.dart
@@ -5,4 +5,5 @@ export 'src/result.dart';
 export 'src/validators/validator.dart';
 export 'src/validator_rule.dart';
 export 'src/validasi.dart';
-export 'src/forms/form.dart';
+export 'src/forms/field_validator.dart';
+export 'src/forms/group_validator.dart';
