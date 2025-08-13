22f57f8e53fa45d19f5d864591a2cc052f61e4bf
albetnov <ryxinfrvr@gmail.com>
2025-08-13 19:26:12 +0700

    feat: add ValidasiEnum class for enum validation and update Validasi interface

---
+++ lib/src/engines/validasi_enum.dart
@@ -0,0 +1,25 @@
+import 'package:validasi/src/engines/validasi_engine.dart';
+import 'package:validasi/src/validasi_result.dart';
+
+class ValidasiEnum extends ValidasiEngine<String> {
+  const ValidasiEnum(this.values);
+
+  final List<String> values;
+
+  @override
+  ValidasiResult<String> validate(dynamic value) {
+    final isValid = values.contains(value);
+
+    return ValidasiResult(
+        errors: isValid
+            ? []
+            : [
+                ValidasiError(
+                  rule: 'Enum',
+                  message: "$value does not belong to the enum",
+                )
+              ],
+        isValid: isValid,
+        data: value);
+  }
+}
--- lib/validasi.dart
+++ lib/validasi.dart
@@ -1,6 +1,7 @@
 library;
 
 import 'package:validasi/src/engines/validasi_engine.dart';
+import 'package:validasi/src/engines/validasi_enum.dart';
 import 'package:validasi/src/engines/validasi_list.dart';
 import 'package:validasi/src/rule.dart';
 import 'package:validasi/src/engines/validasi_scalar.dart';
@@ -11,4 +12,6 @@ class Validasi {
 
   static ValidasiList<T> list<T>(ValidasiEngine<T> itemSchema) =>
       ValidasiList<T>(itemSchema);
+
+  static ValidasiEnum enumValue(List<String> values) => ValidasiEnum(values);
 }
