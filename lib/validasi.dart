c3b9c575c38f23692b066678dde6fb8e61dbd5b6
albetnov <ryxinfrvr@gmail.com>
2025-08-18 09:20:04 +0700

    feat: add cache management to Validasi class for improved performance

--- lib/validasi.dart
+++ lib/validasi.dart
@@ -4,14 +4,28 @@ import 'package:validasi/src/engine/engine.dart';
 import 'package:validasi/src/engine/rule.dart';
 
 class Validasi {
+  static bool withCache = true;
+
+  static T withoutCache<T>(T Function() callback) {
+    final previousState = withCache;
+
+    try {
+      withCache = false;
+      final result = callback();
+      return result;
+    } finally {
+      withCache = previousState;
+    }
+  }
+
   static ValidasiEngine<String> string([List<Rule<String>>? rules]) =>
-      ValidasiEngine(rules: rules);
+      ValidasiEngine(rules: rules, cacheEnabled: withCache);
 
   static ValidasiEngine<List<T>> list<T>([List<Rule<List<T>>>? rules]) =>
-      ValidasiEngine(rules: rules);
+      ValidasiEngine(rules: rules, cacheEnabled: withCache);
 
   static ValidasiEngine<Map<String, T>> map<T>(
       [List<Rule<Map<String, T>>>? rules]) {
-    return ValidasiEngine(rules: rules);
+    return ValidasiEngine(rules: rules, cacheEnabled: withCache);
   }
 }
