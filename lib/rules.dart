ae08713e3edb71ca374947169a5f3ac9006d0c82
albetnov <ryxinfrvr@gmail.com>
2025-08-24 12:44:27 +0700

    feat: add NumberRules class for number validation methods

--- lib/rules.dart
+++ lib/rules.dart
@@ -8,6 +8,7 @@ import 'package:validasi/engine.dart';
 import 'src/rules/string/rules.dart' as string_rules;
 import 'src/rules/iterable/rules.dart' as iterable_rules;
 import 'src/rules/map/rules.dart' as map_rules;
+import 'src/rules/numbers/rules.dart' as number_rules;
 
 class StringRules {
   static string_rules.MinLength minLength(int length, {String? message}) {
@@ -34,5 +35,31 @@ class MapRules {
   static map_rules.HasFields<T> hasFields<T>(
       Map<String, ValidasiEngine<T>> validator) {
     return map_rules.HasFields(validator);
+  }
+}
+
+class NumberRules {
+  static number_rules.Finite<T> finite<T extends num>({String? message}) {
+    return number_rules.Finite(message: message);
+  }
+
+  static number_rules.LessThan<T> lessThan<T extends num>(T value,
+      {String? message}) {
+    return number_rules.LessThan(value, message: message);
+  }
+
+  static number_rules.LessThanEqual<T> lessThanEqual<T extends num>(T value,
+      {String? message}) {
+    return number_rules.LessThanEqual(value, message: message);
+  }
+
+  static number_rules.MoreThan<T> moreThan<T extends num>(T value,
+      {String? message}) {
+    return number_rules.MoreThan(value, message: message);
+  }
+
+  static number_rules.MoreThanEqual<T> moreThanEqual<T extends num>(T value,
+      {String? message}) {
+    return number_rules.MoreThanEqual(value, message: message);
   }
 }
---
+++ lib/src/rules/numbers/rules.dart
@@ -0,0 +1,5 @@
+export 'finite.dart';
+export 'less_than_equal.dart';
+export 'less_than.dart';
+export 'more_than_equal.dart';
+export 'more_than.dart';
