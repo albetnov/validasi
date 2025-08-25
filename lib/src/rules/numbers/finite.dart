7608af9c5268441b45d777e4ca688f63b9cc6f8f
albetnov <ryxinfrvr@gmail.com>
2025-08-25 15:12:01 +0700

    refactor: update Finite rule to use double type for improved type safety

--- lib/src/rules/numbers/finite.dart
+++ lib/src/rules/numbers/finite.dart
@@ -2,11 +2,11 @@ import 'package:validasi/src/engine/context.dart';
 import 'package:validasi/src/engine/error.dart';
 import 'package:validasi/src/engine/rule.dart';
 
-class Finite<T extends num> extends Rule<T> {
+class Finite extends Rule<double> {
   const Finite({super.message});
 
   @override
-  void apply(ValidationContext<T> context) {
+  void apply(ValidationContext<double> context) {
     if (context.requireValue.isInfinite == false) {
       context.addError(ValidationError(
         rule: 'finite',
