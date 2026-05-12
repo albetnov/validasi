d6250c6c1350c02ad5caa87f7497dcb2bec346ff
albetnov <ryxinfrvr@gmail.com>
2026-05-12 13:47:43 +0700

    refactor(state): remove stop and addError methods from ValidationState class

--- packages/validasi/lib/src/engine/state.dart
+++ packages/validasi/lib/src/engine/state.dart
@@ -3,12 +3,4 @@ import 'package:validasi/src/engine/error.dart';
 class ValidationState {
   final List<ValidationError> errors = [];
   bool isStopped = false;
-
-  void stop() {
-    isStopped = true;
-  }
-
-  void addError(ValidationError error) {
-    errors.add(error);
-  }
 }
